a0ad34b0d57e10e32ca96379e9648c8f0659e970
Kashif <darkashif1974@gmail.com>
2025-08-29 18:32:05 +0530

    division

---
+++ main.py
@@ -0,0 +1,7 @@
+g1 = input('enter the value of first number: ')
+g2 = input('enter the value of second number ')
+# contverting string to interger form
+k1=int(g1)
+k2=int(g2)
+div=(k1)/(k2)
+print("division :",div)
