4e52615a5d7899d9c9697a45caa4ce0d1cf4d5a9
Kashif <darkashif1974@gmail.com>
2025-08-29 18:33:43 +0530

    multiplication

---
+++ main.py
@@ -0,0 +1,7 @@
+g1 = input('enter the value of first number: ')
+g2 = input('enter the value of second number ')
+# contverting string to interger form
+k1=int(g1)
+k2=int(g2)
+multi=(k1)*(k2)
+print("multiplication :",multi)
