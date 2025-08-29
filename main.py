431cbfc22e6ef1306ead82fc64ad6ee661b8a778
Kashif <darkashif1974@gmail.com>
2025-08-29 18:54:36 +0530

    HELLO WELCOME PYTHON

---
+++ main.py
@@ -0,0 +1,16 @@
+
+n1=input('Enter your first name :')
+n2=input('Enter your second name :')
+f1=(n1+" "+n2)
+print('hello,'+ (f1) +('!')+ ('\twelcome') + ('\tto') + ('\tthe')+ ('\tpython')+ ('\tprogram.'))
+
+
+
+
+
+
+
+
+
+
+
